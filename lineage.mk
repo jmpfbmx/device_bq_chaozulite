b40f1d91ec5fadc5bac124026d18c6b3736387b5
jmpfbmx <jmpf.bmx@gmail.com>
2020-02-24 21:23:24 +0100

    chaozulite: lineage-ify

---
+++ lineage.mk
@@ -0,0 +1,46 @@
+#
+# Copyright (C) 2016 The CyanogenMod Project
+# Copyright (C) 2017 The LineageOS Project
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+# Inherit framework first
+$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
+
+# Inherit from chaozulite device
+$(call inherit-product, device/bq/chaozulite/device.mk)
+
+# Inherit from common device
+$(call inherit-product, device/bq/msm8937-common/msm8937.mk)
+
+# Inherit some common LineageOS stuff.
+$(call inherit-product, vendor/cm/config/common_full_phone.mk)
+
+# Set those variables here to overwrite the inherited values.
+BOARD_VENDOR := bq
+PRODUCT_DEVICE := chaozulite
+PRODUCT_NAME := lineage_chaozulite
+PRODUCT_BRAND := bq
+PRODUCT_MODEL := Aquaris U Lite
+PRODUCT_MANUFACTURER := bq
+
+# Set product device & name
+PRODUCT_BUILD_PROP_OVERRIDES += \
+   PRODUCT_NAME=chaozulite
+
+PRODUCT_GMS_CLIENTID_BASE := android-bq
+
+PRODUCT_BUILD_PROP_OVERRIDES += \
+    BUILD_FINGERPRINT=bq/chaozulite/chaozulite:7.1.2/NMF26F/1109:user/release-keys \
+    PRIVATE_BUILD_DESC="chaozulite-user 7.1.2 NMF26F 1109 release-keys"
